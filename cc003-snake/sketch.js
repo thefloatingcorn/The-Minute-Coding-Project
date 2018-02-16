90f0c997021a5b9ba9775293ccf58a006a74c78e
thefloatingcorn <yuhui.yaocn@gmail.com>
2018-02-16 14:32:34 +0000

    rev dir ctrl

--- cc003-snake/sketch.js
+++ cc003-snake/sketch.js
@@ -14,9 +14,9 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
-		s.dir(0,1);
-	} else if (keyCode === DOWN_ARROW) {
 		s.dir(0,-1);
+	} else if (keyCode === DOWN_ARROW) {
+		s.dir(0,1);
 	} else if (keyCode === LEFT_ARROW) {
 		s.dir(-1,0);
 	} else if (keyCode === RIGHT_ARROW) {
