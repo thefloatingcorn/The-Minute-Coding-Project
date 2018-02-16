ed0736b6695132089b1a3297c43e4f80e6d35e35
thefloatingcorn <yuhui.yaocn@gmail.com>
2018-02-16 12:09:35 +0000

    Update sketch.js

--- cc003-snake/sketch.js
+++ cc003-snake/sketch.js
@@ -1,7 +1,25 @@
 function setup() {
-  // put setup code here
+    createCanvas(600,600);
+    s = new Snake();
 }
 
 function draw() {
-  // put drawing code here
+    background(51);
+    s.update();
+    s.show();
+}
+
+function Snake(){
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.update = function() {
+      this.x = this.x + this.xspeed;
+      this.y = this.y + this.yspeed;
+    }
+    this.show = function() {
+      fill(255);
+      rect(this.x, this.y, 10, 10);
+    }
 }
