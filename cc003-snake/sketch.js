ccde3618b523dcf586ba5ad7f87ff4716b1e1303
thefloatingcorn <yuhui.yaocn@gmail.com>
2018-02-23 22:45:03 +0000

    can inc tail

--- cc003-snake/sketch.js
+++ cc003-snake/sketch.js
@@ -1,15 +1,32 @@
 var s;
 var scl = 20; 
+var food;
 
 function setup() {
     createCanvas(600,600);
     s = new Snake();
+	frameRate(10);
+	pickLocation();
 }
 
 function draw() {
     background(51);
     s.update();
     s.show();
+	
+	if (s.eat(food) == true) {
+		pickLocation();
+	}
+	
+	fill(255, 0, 100);
+	rect(food.x, food.y, scl, scl);
+}
+
+function pickLocation() {
+	var cols = floor(width/scl);
+	var rows = floor(height/scl);
+	food = createVector(floor(random(cols)), floor(random(rows)));
+	food.mult(scl);
 }
 
 function keyPressed() {
@@ -28,16 +45,41 @@ function Snake(){
     this.y = 0;
     this.xspeed = 1;
     this.yspeed = 0;
+	this.total = 0;
+	this.tail = [];
+	
+	this.eat = function(pos) {
+		var d = dist(this.x, this.y, pos.x, pos.y);
+		if (d < 1) {
+			this.total++;
+			return true;
+		} else {
+			return false;
+		}
+	}
 	this.dir = function(x, y) {
 		this.xspeed = x;
 		this.yspeed = y;
 	}
     this.update = function() {
-      this.x = this.x + this.xspeed;
-      this.y = this.y + this.yspeed;
+		if (this.total === this.tail.length) {
+			for (var i = 0; i < this.tail.length-1; i++) {
+				this.tail[i] = this.tail[i+1];
+			}
+		}
+		this.tail[this.total-1] = createVector(this.x, this.y);
+		
+        this.x = this.x + this.xspeed*scl;
+        this.y = this.y + this.yspeed*scl;
+	  
+	    this.x = constrain(this.x, 0, width - scl);
+	    this.y = constrain(this.y, 0, height - scl);
     }
     this.show = function() {
-      fill(255);
-      rect(this.x, this.y, 10, 10);
-    }
+        fill(255);
+		for (var i = 0; i < this.tail.length; i++) { 
+			rect(this.tail[i].x, this.tail[i].y, scl, scl);
+		}
+		rect(this.x, this.y, scl, scl);
+    }	
 }
