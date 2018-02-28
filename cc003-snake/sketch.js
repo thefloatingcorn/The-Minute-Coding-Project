532b16cb8321b049a7c25ecb4fc4aea4488a59d7
thefloatingcorn <yuhui.yaocn@gmail.com>
2018-02-28 12:37:06 +0000

    Death on Edge

--- cc003-snake/sketch.js
+++ cc003-snake/sketch.js
@@ -64,14 +64,31 @@ function Snake(){
 	}
 	
 	this.death = function() {
-		for (var i = 0; i< this.tail.length; i++) {
-			var pos = this.tail[i];
-			var d = dist(this.x, this.y, pos.x, pos.y);
-			if (d < 1) {
-				console.log('starting over'); 
-				this.total = 0;
-				this.tail = [];
+		//console.log(this.x + ',' + this.y); 
+		if ( (this.x <= width - scl) && (this.x >= 0) 
+		&& (this.y <= height - scl) && (this.y >= 0) ) {
+			for (var i = 0; i< this.tail.length; i++) {
+				var pos = this.tail[i];
+				var d = dist(this.x, this.y, pos.x, pos.y);
+				if (d < 1) {
+					console.log('starting over'); 
+					this.total = 0;
+					this.tail = [];
+					this.x = 0;
+					this.y = 0;
+					this.xspeed = 1;
+					this.yspeed = 0;				
+				}
 			}
+		}
+		else {
+			console.log('starting over'); 
+			this.total = 0;
+			this.tail = [];
+			this.x = 0;
+			this.y = 0;
+			this.xspeed = 1;
+			this.yspeed = 0;		
 		}
 	}
 	
@@ -87,8 +104,8 @@ function Snake(){
         this.x = this.x + this.xspeed*scl;
         this.y = this.y + this.yspeed*scl;
 	  
-	    this.x = constrain(this.x, 0, width - scl);
-	    this.y = constrain(this.y, 0, height - scl);
+//	    this.x = constrain(this.x, 0, width - scl);
+//	    this.y = constrain(this.y, 0, height - scl);
     }
 	
     this.show = function() {
