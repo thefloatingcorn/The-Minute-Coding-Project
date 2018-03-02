6e51c51e5f4eed98c19012a63f978fcd6388a1d5
thefloatingcorn <yuhui.yaocn@gmail.com>
2018-03-02 10:56:34 +0000

    before combine

--- cc094-2048/sketch.js
+++ cc094-2048/sketch.js
@@ -8,10 +8,10 @@ function setup() {
 		[0,0,0,0],
 		[0,0,0,0]
 	];
-	console.table(grid);
+//	console.table(grid);
 	addNumber();
 	addNumber();
-	console.table(grid);
+//	console.table(grid);
 }
 
 function addNumber() {
@@ -33,6 +33,45 @@ function addNumber() {
 		
 }
 
+//one "move"
+function keyPressed() {
+	if (key == ' ') {
+		for (let i = 0; i < 4; i++) {
+			grid[i] = slide(grid[i])
+		}
+	}
+	addNumber();
+}
+
 function draw() {
-	background(0);
+	background(255);
+	drawGrid();
+}
+
+function slide(row) {
+	let arr = row.filter(val => val);
+	let missing = 4 - arr.length;
+	let zeros = Array(missing).fill(0);
+	arr = arr.concat(zeros);
+	return arr;
+}
+
+function drawGrid() {
+	let w = 100;
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			noFill();
+			strokeWeight(2);
+			stroke(0);
+			rect(i*w,j*w,w,w);
+			let val = grid[i][j];
+			if (val !== 0) {
+				textAlign(CENTER, CENTER);
+				textSize(64);
+				fill(0);
+				noStroke();
+				text(val, i*w+w/2, j*w+w/2);
+			}			
+		}
+	}	
 }
