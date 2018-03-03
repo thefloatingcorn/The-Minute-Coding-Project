fe1dca47302931bca3c8ff55443a68d9a6d0304a
thefloatingcorn <yuhui.yaocn@gmail.com>
2018-03-03 15:57:20 +0000

    to right

--- cc094-2048/sketch.js
+++ cc094-2048/sketch.js
@@ -1,13 +1,17 @@
 let grid;
 
-function setup() {
-    createCanvas(400,400);
-	grid =  [
+function blankGrid() {
+	return 	[
 		[0,0,0,0],
 		[0,0,0,0],
 		[0,0,0,0],
 		[0,0,0,0]
 	];
+}
+
+function setup() {
+    createCanvas(400,400);
+	grid = blankGrid();
 //	console.table(grid);
 	addNumber();
 	addNumber();
@@ -45,12 +49,7 @@ function compare(a,b) {
 }
 
 function copyGrid(grid) {
-	let extra =  [
-		[0,0,0,0],
-		[0,0,0,0],
-		[0,0,0,0],
-		[0,0,0,0]
-	];
+	let extra = blankGrid();
 	for (let i = 0; i < 4; i++) {
 		for (let j = 0; j < 4; j++) {
 			extra[i][j] = grid[i][j]
@@ -59,21 +58,35 @@ function copyGrid(grid) {
 	return extra;
 }
 	
-function flip(grid) {
+function flipGrid(grid) {
 	for (let i = 0; i < 4; i++) {
 		grid[i].reverse();
 	}
+}
+
+function rotateGrid(grid) {
+	let newGrid = blankGrid();
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			newGrid[i][j] = grid[j][i];
+		}
+	}
+	return newGrid;
 }
 	
 	
 //one "move"
 function keyPressed() {
 	let flipped = false;
+	let rotated = false;
 	if (keyCode === DOWN_ARROW){
 		// DO NOTHING
 	} else if (keyCode === UP_ARROW) {
-		flip(grid);
+		flipGrid(grid);
 		flipped = true;
+	} else if (keyCode === RIGHT_ARROW) {
+		grid = rotateGrid(grid);
+		rotated = true;
 	}
 	let past = copyGrid(grid);
 	for (let i = 0; i < 4; i++) {
@@ -82,8 +95,14 @@ function keyPressed() {
 	let changed = compare(past, grid);
 	
 	if (flipped) {
-		flip(grid);
+		flipGrid(grid);
 	}
+	if (rotated) {
+		grid = rotateGrid(grid);
+		grid = rotateGrid(grid);
+		grid = rotateGrid(grid);
+	}
+	
 	if (changed) {
 		addNumber();
 	}
