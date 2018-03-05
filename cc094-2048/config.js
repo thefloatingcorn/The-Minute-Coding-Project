f796665be46cd4413d28905d874de079b8fe0ea0
thefloatingcorn <yuhui.yaocn@gmail.com>
2018-03-05 16:08:11 +0000

    seperate files and add size/color

---
+++ cc094-2048/config.js
@@ -0,0 +1,52 @@
+// $ct-blue: #2494C1
+// $ct-green: #49BB6C
+// $ct-purple: #9659A7
+// $ct-red: #F35956
+// $ct-yellow: #F1C500
+
+let colorsSizes = {
+	"2": {
+		size: 64, 
+		color: "#F35956"
+	}, 	
+	"4": {
+		size: 64, 
+		color: "#F35956"
+	}, 	
+	"8": {
+		size: 64, 
+		color: "#49BB6C"
+	}, 	
+	"16": {
+		size: 64, 
+		color: "#2494C1"
+	}, 	
+	"32": {
+		size: 64, 
+		color: "#9659A7"
+	}, 	
+	"64": {
+		size: 64, 
+		color: "#F1C500"
+	}, 	
+	"128": {
+		size: 36, 
+		color: "#FF5956"
+	}, 	
+	"256": {
+		size: 36, 
+		color: "#F1C5FF"
+	}, 	
+	"512": {
+		size: 36, 
+		color: "#99BB9C"
+	}, 		
+	"1024": {
+		size: 18, 
+		color: "#24EFC1"
+	}, 	
+	"2048": {
+		size: 18, 
+		color: "#A659A9"
+	}
+}
